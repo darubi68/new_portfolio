c145a345a680f0599007459a2f3ab79d04e5a68e
Jeongmoon <darubi68@naver.com>
2023-11-07 22:54:21 +0900

    스크롤 이벤트 추가

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,3 @@
-
-
 function setMainSVG () {
     if(window.innerWidth > window.innerHeight - 100) {
         document.querySelector('.main-svg').style.width = 'auto';
@@ -7,6 +5,34 @@ function setMainSVG () {
     } else {
         document.querySelector('.main-svg').style.width = '100%';
         document.querySelector('.main-svg').style.height = 'auto'; 
+    }
+}
+
+function portfolioScroll () {
+    const sceneBackground = document.querySelector('.portfolio-background');
+    const scrollSceneHeight = document.getElementById('portfolio').scrollHeight;
+    const normalScene = document.querySelectorAll('.normal-scene');
+    let normalSceneHeight = 0;
+
+    for(let i=0; i < normalScene.length; i++) {
+        normalSceneHeight += normalScene[i].scrollHeight;
+    }
+
+    const scrollRatio = ((window.pageYOffset - normalSceneHeight)-(scrollSceneHeight - window.innerHeight)) / (scrollSceneHeight - window.innerHeight);
+
+
+    if(window.pageYOffset < normalSceneHeight) {
+        sceneBackground.style.transform = 'translate3d(0, -100%, 0)';
+        sceneBackground.style.opacity = 0;
+        sceneBackground.classList.remove('scroll-on');
+    } else if(window.pageYOffset > normalSceneHeight && window.pageYOffset < (normalSceneHeight + (scrollSceneHeight - window.innerHeight))) {
+        sceneBackground.style.transform = `translate3d(0, ${Math.round(scrollRatio*100)}%, 0)`;
+        sceneBackground.style.opacity = 1;
+        sceneBackground.classList.add('scroll-on');
+    } else if(window.pageYOffset > (normalSceneHeight + (scrollSceneHeight - window.innerHeight))){
+        sceneBackground.style.transform = 'translate3d(0, 0, 0)';
+        sceneBackground.style.opacity = 1;
+        sceneBackground.classList.remove('scroll-on');
     }
 }
 
@@ -30,7 +56,7 @@ window.addEventListener('load', () => {
 });
 
 window.addEventListener('scroll', () => {
-
+    portfolioScroll();
 });
 
 window.addEventListener('orientationchange', ()=> {
@@ -40,3 +66,5 @@ window.addEventListener('orientationchange', ()=> {
 window.addEventListener('resize', () => {
     setMainSVG();
 });
+
+
